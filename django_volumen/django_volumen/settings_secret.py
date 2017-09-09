8d434052b33137ad25482bd13ae7765f63e53854
Carlos Gomes <kmartinho8@gmail.com>
2017-09-09 23:43:29 +0200

    added the django hello world app

---
+++ django_volumen/django_volumen/settings_secret.py
@@ -0,0 +1,4 @@
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY_HIDDEN = 'ijvxw)(xcbhzaf8wbqa6gl!wr1&nu%(@8ei-w(3u56pxy7jk9m'
---
+++ django_volumen/volumen_main_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class VolumenMainAppConfig(AppConfig):
+    name = 'volumen_main_app'

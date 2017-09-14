5a2837749a16f29d12122895e3a80c9c071448e6
Carlos Gomes <kmartinho8@gmail.com>
2017-09-14 22:19:12 +0200

    added name space to the views

--- django_volumen/volumen_main_app/views.py
+++ django_volumen/volumen_main_app/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'main/index.html'
+
+# def home(request):
+#     return render(request, "main/index.html")
